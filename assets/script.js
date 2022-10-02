99965b7b025b555e9c941ec074f575b16317a7ac
nftgoat <justinross28@gmail.com>
2022-10-02 08:14:55 -0700

    added inital window alert and prompt

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  document.querySelector("#password").value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//generatePassword function
+  alert ("Throughout these pop-ups, Please fill out your password criteria.")
+
+//Define length criteria
+  var length = prompt ('Choose the length of your password.\nMust be at least 8 characters.\nMust not exceed 128 characters.');
+  
