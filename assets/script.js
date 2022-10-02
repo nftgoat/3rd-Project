1e220e5a4d78a9879af6d47bbb8928f34b699c4d
nftgoat <justinross28@gmail.com>
2022-10-02 09:45:00 -0700

    added additional window  confirmations

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ generateBtn.addEventListener("click", writePassword);
   alert ("Throughout these pop-ups, Please fill out your password criteria.")
 
 //Define length criteria
-  var length = prompt ('Choose the length of your password.\nMust be at least 8 characters.\nMust not exceed 128 characters.');
+  var length = prompt ("Choose the length of your password.\nMust be at least 8 characters.\nMust not exceed 128 characters.");
 
 //Check length criteria
   var passwordInvalid = true;
@@ -23,7 +23,18 @@ generateBtn.addEventListener("click", writePassword);
       if (length > 7 && length < 129){
       {  passwordInvalid = false;} 
       }else {
-        alert('Your input was invalid. Try again.');
-        length = prompt('Choose the length of your password.\nMust be at least 8 characters.\nMust not exceed 128 characters.');
+        alert("Your input was invalid. Try again.");
+        length = prompt("Choose the length of your password.\nMust be at least 8 characters.\nMust not exceed 128 characters.");
     } 
 }
+// change value of length to a number
+var numLength = Number(length);
+
+alert("You chose: " + length);
+
+ // check choices for character options
+ var lowerCase = confirm("Do you want your password to include lower case letters?");
+ var upperCase = confirm("Do you want your password to include upper case letters?");
+ var numeric = confirm("Do you want your password to include numbers?");
+ var specialCharacter = confirm("Do you want your password to include special characters? \nEx: !, $, #, ?, @");
+ alert("You chose the following options: \n Lower Case Letters: " + lowerCase + "\n Upper Case Letters: " + upperCase + "\n Numbers: " + numeric + "\n Special Characters: " + specialCharacter);
